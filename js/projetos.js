5abc2d9f82633eac836485a1ba071c607d8aafc3
Saiki <saikihector@gmail.com>
2026-08-07 20:17:35 -0300

    Página "Projetos"
    Criação inicial da página 'Projetos', onde poderá ser feito a exposição de projetos em produção e já finalizados.

---
+++ js/projetos.js
@@ -0,0 +1,44 @@
+class HamburguerNavBar {
+    constructor(hamburguer, navList, navLinks) {
+        this.hamburguer = document.querySelector(hamburguer);
+        this.navList = document.querySelector(navList);
+        this.navLinks = document.querySelectorAll(navLinks);
+        this.activeClass = "active";
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    animateLinks() {
+        this.navLinks.forEach((links, index) => {
+            links.style.animation
+            ? (links.style.animation = "")
+            : (links.style.animation = `navLinksFade 0.5s ease forwards ${index / 7 + 0.3}s`)
+        });
+    }
+
+    handleClick() {
+        console.log(this);
+        this.navList.classList.toggle(this.activeClass);
+        this.hamburguer.classList.toggle(this.activeClass);
+        this.animateLinks()
+    }
+
+    addClickEvent() {
+        this.hamburguer.addEventListener("click", this.handleClick);
+    }
+
+    init() {
+        if (this.hamburguer) {
+            this.addClickEvent();
+        }
+        return this;
+    }
+}
+
+const hamburguerNavBar = new HamburguerNavBar(
+    ".hamburguer",
+    ".nav-list",
+    ".nav-list li",
+);
+
+hamburguerNavBar.init();
